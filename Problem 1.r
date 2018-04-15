1cb7edbfe52b1db29242db7add1d0efcb33de616
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-15 17:14:36 -0500

    Create Problem 1.r

---
+++ Problem 1.r
@@ -0,0 +1,17 @@
+# Euler Problem 1
+# Derek R. Blanchette
+
+total <- 0
+
+for (i in seq(1,999)){
+    mod3 <- i %% 3
+    mod5 <- i %% 5
+    
+    both <- (mod3 ==0 && mod5 == 0)
+    either <- (mod3 == 0 || mod5 == 0)
+    if (either || both) {
+        total = total + i
+        }
+}
+
+print(total)
