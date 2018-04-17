1034d8fcc58f0785fbda7b87e230a8bd0dec74b4
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-17 11:22:50 -0500

    Create Problem 4.r

---
+++ Problem 4.r
@@ -0,0 +1,36 @@
+# Euler Project Problem 4
+# Derek R. Blanchette
+
+pals <- NULL
+
+#this search needs to be triangular.
+#from 1 to 999
+#then 1 to 998
+#then 1 to 997
+#or something like that 
+
+for (i in seq(999,100)){
+    for (j in seq(999,100)){
+        
+        word <-paste(i*j)
+        
+        if (nchar(word) %% 2 == 0 ) {
+            a <- substr(word, 1, nchar(word)/2)
+            b <- substr(word, nchar(word)/2 + 1, nchar(word))
+            
+        } else {
+            a <- substr(word, 1, (nchar(word) - 1)/2)
+            b <- substr(word, (nchar(word) - 1)/2 + 2, nchar(word))
+        }
+        
+        c <- paste(rev(strsplit(b, split = "")[[1]]), collapse = "") 
+        
+        if (a == c) {
+            pals <- rbind(pals, cbind(i, j, i*j))
+        }
+    }
+}
+
+#just let it run for a while then sort the results.
+#arrange(pals, c)
+#the answer is 913*993 906609r
