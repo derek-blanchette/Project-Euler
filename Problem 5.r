9a9928e048e9d5caec6d540064616773fd04afaa
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-17 11:23:50 -0500

    Create Problem 5.r

---
+++ Problem 5.r
@@ -0,0 +1,16 @@
+# Euler Project Problem 5
+# 
+# Derek R. Blanchette
+
+dividend <- 126717199
+divisors <- seq(1:20)
+check.total <- 1
+
+repeat{
+  check.total <- sum(dividend %% divisors)
+  if(check.total == 0){break}
+  dividend <- dividend + 1 
+}
+
+print(dividend)
+
