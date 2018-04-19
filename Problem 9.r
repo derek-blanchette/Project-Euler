dae5a31cd8bcae0aeea7a6c6f596a9535453f4f1
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-18 20:21:46 -0500

    Create Problem 9.r

---
+++ Problem 9.r
@@ -0,0 +1,17 @@
+# Euler Project #9
+# Derek R. Blanchette
+
+
+for (i in seq(146,1000)){
+    for (j in seq(i+1,1000)){
+        for (k in seq(j+1,1000)){
+            if (i*i+j*j == k*k ) {
+                print(i + j + k)
+                if ( i + j + k == 1000){
+                    print(cbind(i,j,k))
+                }
+            }
+        }
+    }
+    print(i)
+}
