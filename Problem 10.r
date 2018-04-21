fc47c9fdc310d70a986c28e965dc13cb5aedad2d
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-21 10:34:43 -0500

    Create Problem 10.r

---
+++ Problem 10.r
@@ -0,0 +1,18 @@
+sieve <- function(n)
+{
+  n <- as.integer(n)
+  primes <- rep(TRUE, n)
+  primes[1] <- FALSE
+  last.prime <- 2L
+  for(i in last.prime:floor(sqrt(n)))
+  {
+    primes[seq.int(2L*last.prime, n, last.prime)] <- FALSE
+    last.prime <- last.prime + min(which(primes[(last.prime+1):n]))
+  }
+  which(primes)
+}
+
+a <- sieve(2000000)
+
+#Give the sum of all primes up to 2 million
+sum(as.numeric(a))
